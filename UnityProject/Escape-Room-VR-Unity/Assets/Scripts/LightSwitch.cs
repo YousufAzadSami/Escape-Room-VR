4a1b8d430e530902543a84ca43a0f2f5d9b5df12
Yousuf Azad (Sami) <sami1592@gmail.com>
2018-12-30 18:42:12 +0100

    Trigger detected from LightSwitch that player has entered the zone.

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag == "Player")
+        {
+            Debug.Log("Object Name: " + collider.transform.name);
+
+			// if player interacts by pressing by any key 
+			// OR 
+			// just by coming in the trigger zone following actions will be performed
+			
+        }
+    }
+}
