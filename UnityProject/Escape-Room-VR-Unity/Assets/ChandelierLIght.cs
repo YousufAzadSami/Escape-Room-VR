7cef1060a71a7377d02fe8a653ca979653e71fa8
Yousuf Azad (Sami) <sami1592@gmail.com>
2018-12-31 14:54:11 +0100

    light changes with keypress E

--- UnityProject/Escape-Room-VR-Unity/Assets/ChandelierLIght.cs
+++ UnityProject/Escape-Room-VR-Unity/Assets/ChandelierLIght.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class ChandelierLIght : MonoBehaviour {
 
+	// public Color[] lightColors = {Color.red, Color.green, Color.blue, Color.white};
+	List<Color> lightColors = new List<Color>();
+	public int currentColorIndex;
+
 	// Use this for initialization
 	void Start () {
-		
+		lightColors.Add(Color.red);
+		lightColors.Add(Color.green);
+		lightColors.Add(Color.blue);
+		lightColors.Add(GetComponent<Light>().color);
+		// Debug.Log("Length : " + lightColors.Count);
+
+		currentColorIndex = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void ChangeLight()
+	{
+		// Debug.Log("Current Index: " + currentColorIndex);
+
+		Light light = GetComponent<Light>();
+		currentColorIndex++;
+
+		// Debug.Log("Current Index: " + currentColorIndex);
+
+		if(currentColorIndex == lightColors.Count)
+		{
+			currentColorIndex = 0;
+		}
+		light.color = lightColors[currentColorIndex];
 	}
 }
--- UnityProject/Escape-Room-VR-Unity/Assets/Scripts/LightSwitch.cs
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/LightSwitch.cs
@@ -17,21 +17,28 @@ public class LightSwitch : MonoBehaviour {
 		
 	}
 
-	public void OnTriggerEnter(Collider collider)
+	public void OnTriggerStay(Collider collider)
     {
         if (collider.transform.tag == "Player")
         {
-            Debug.Log("Object Name: " + collider.transform.name);
+			// Debug.Log("Object Name: " + collider.transform.name);
 
-			// if player interacts by pressing by any key 
-			// OR 
-			// just by coming in the trigger zone following actions will be performed
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				// if player interacts by pressing by any key 
+				// OR 
+				// just by coming in the trigger zone following actions will be performed
+				// idea #1 4 different lights can be found by swithcing off(bottom), on(top) the light
+				// and Vive controller key press will determine wihch will be shown
+				// idea #2 4 different lights can be found by swithching off(bottom), on(top), right and left
+				// and Vive controller direction will determine which will be shown
 
-			// move the switch handle
-			SwithcHandleAnimation();
-			// and 
-			// turn differnt lights (switch between them)
-			SwitchDifferentLights();
+				// move the switch handle
+				SwithcHandleAnimation();
+				// and 
+				// turn differnt lights (switch between them)
+				SwitchDifferentLights();
+			}
         }
     }
 
@@ -42,8 +49,7 @@ public class LightSwitch : MonoBehaviour {
 	}
 
 	private void SwitchDifferentLights()
-	{
-		// TODO
-		Debug.Log("TODO");
+	{		
+		chandelierLight.GetComponent<ChandelierLIght>().ChangeLight();
 	}
 }
