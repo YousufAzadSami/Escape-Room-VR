910bf737e9d1645da576b8bd3cadc782f470abbc
Yousuf Azad (Sami) <sami1592@gmail.com>
2019-01-01 12:37:32 +0100

    adds a class to store puzzle related info. more to be added later

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour {
+
+	public static int PASS_CODE_01 = 123;
+	public static int PASS_CODE_02 = 456;
+	public static int PASS_CODE_03 = 789;
+
+	public enum PuzzleLevel
+	{
+		One, Two, Three, Four
+	}
+
+	public static PuzzleLevel currentPuzzleLevel;
+
+	// Use this for initialization
+	void Start () {
+		currentPuzzleLevel = PuzzleLevel.One;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
