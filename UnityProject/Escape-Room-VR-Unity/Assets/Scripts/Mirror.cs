7af88f3f81a9c77507a73edb1359ae6b9cbbe111
Yousuf Azad <sami1592@gmail.com>
2019-01-25 14:05:40 +0100

    adds rough puzzle 2 functionality with dummy object

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/Mirror.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Mirror : MonoBehaviour {
+
+    public GameObject normalMirror;
+    public GameObject shatterMirror;
+    public GameObject card;
+
+    // Use this for initialization
+    void Start () {
+        shatterMirror.SetActive(false);
+        card.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Valve.VR.InteractionSystem.Throwable _object = collision.transform.GetComponent<Valve.VR.InteractionSystem.Throwable>();
+
+        if (_object)
+        {
+            // set th
+            normalMirror.SetActive(false);
+            shatterMirror.SetActive(true);
+            card.SetActive(true);
+        }
+
+    }
+}
