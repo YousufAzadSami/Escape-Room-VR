9fd38d00144ca3ade34a4a524bcaa2a4643b77a5
Yousuf Azad <sami1592@gmail.com>
2019-01-25 11:44:37 +0100

    adds input test and it works.

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/InputTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class InputTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float x = SteamVR_Input._default.inActions.Squeeze.GetAxis(SteamVR_Input_Sources.Any);
+        if (x > 0)
+        {
+            Debug.Log("Squeeze : " + x);
+        }
+        
+	}
+}
