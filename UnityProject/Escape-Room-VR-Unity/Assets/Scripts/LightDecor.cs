63ec93ac9a272f06a53973eacab55f53b75117d3
Yousuf Azad <sami1592@gmail.com>
2019-01-25 14:30:15 +0100

    sets up lightDecor as separate prefab, the whole group and separate ones. Parents card prefab to a separate parent. Adds light decor to interact

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/LightDecor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDecor : MonoBehaviour {
+
+    public GameObject card;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Valve.VR.InteractionSystem.Throwable _object = collision.transform.GetComponent<Valve.VR.InteractionSystem.Throwable>();
+
+        if (_object)
+        {
+            // set this object to deactive
+            gameObject.SetActive(false);
+            // set shattered version to active
+            
+            // set card to active
+            card.SetActive(true);
+        }
+
+    }
+}
