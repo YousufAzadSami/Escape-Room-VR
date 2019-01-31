d2b93ce38aa2c14ddccb93660162c0b10bed5ed1
Yousuf Azad (Sami) <yazad@techfak.uni-bielefeld.de>
2019-01-31 17:58:41 +0100

    numpad : couldn't scale it up. failed attempt. adds animation to do that.

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/NumpadScale.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumpadScale : MonoBehaviour {
+
+    private Vector3 targetBig;
+    private Vector3 targetOriginal;
+
+    private Vector3 target;
+
+    // Use this for initialization
+    void Start () {
+        targetBig = new Vector3(1, 1, 1);
+        targetOriginal = transform.localScale;
+
+        target = targetOriginal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((transform.localScale.x - target.x) > .01f)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, target, 5.0f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("OnTriggerEnter");
+        //Debug.Log("Object Name: " + other.transform.name);
+        if (other.CompareTag("Hand"))
+        {
+            //transform.localScale = Vector3.Lerp(targetBig, transform.localScale, 2.0f);
+            target = targetBig;
+            Debug.Log("Inside");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("OnTriggerEnter");
+        //Debug.Log("Object Name: " + other.transform.name);
+        if (other.CompareTag("Hand"))
+        {
+            //transform.localScale = Vector3.Lerp(targetOriginal, transform.localScale, 2.0f);
+            target = targetOriginal;
+        }
+    }
+}
