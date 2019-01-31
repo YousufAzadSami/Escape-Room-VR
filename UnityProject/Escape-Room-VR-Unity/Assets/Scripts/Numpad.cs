276546d1018fe2307461cce931faf8f6a9a105d5
Yousuf Azad (Sami) <yazad@techfak.uni-bielefeld.de>
2019-01-31 16:31:59 +0100

    numpad : adds numpad script to buttons to detect which number is pressed and communitcate that to puzzlemanager

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/Numpad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Numpad : MonoBehaviour {
+
+    public int number;
+    private GameObject puzzleManager;
+
+	// Use this for initialization
+	void Start () {
+        puzzleManager = GameObject.Find("PuzzleManager");
+
+        if (! puzzleManager)
+        {
+            Debug.Log("PuzzleManager not found");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NumberButtonPressed()
+    {
+        Debug.Log("Button pressed : " + number);
+
+    }
+}
