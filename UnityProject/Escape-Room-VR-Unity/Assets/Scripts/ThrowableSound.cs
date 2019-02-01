1eaf8276343a0226d3c863cb50d9564e57184331
sami <sami1592@gmail.com>
2019-02-01 20:45:21 +0100

    add sound to throwable object failed

---
+++ UnityProject/Escape-Room-VR-Unity/Assets/Scripts/ThrowableSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableSound : MonoBehaviour {
+
+    AudioClip audioClip;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioClip = Resources.Load("Assets/Audio/ObjectDrop.wav") as AudioClip;
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Thorowabel Sound");
+        float audioLevel = collision.relativeVelocity.magnitude / 10.0f;
+        audioSource.PlayOneShot(audioClip, audioLevel);
+    }
+}
